d37795b1faf327352f7c9bd281a6760be7141eec
Kai Hossbach <paranoiax89@googlemail.com>
2013-08-14 18:17:18 +0200

    added storyboard support

---
+++ src/menu.lua
@@ -0,0 +1,121 @@
+-----------------------------------------------------------------------------------------
+--
+-- menu.lua
+--
+-----------------------------------------------------------------------------------------
+
+local storyboard = require( "storyboard" )
+local scene = storyboard.newScene()
+
+-- include Corona's "widget" library
+local widget = require "widget"
+
+--------------------------------------------
+
+-- forward declarations and other locals
+local playBtn
+
+-- 'onRelease' event listener for playBtn
+local function onPlayBtnRelease()
+	
+	-- go to level1.lua scene
+	storyboard.gotoScene( "level1", "zoomInOutFade", 500)
+	
+	return true	-- indicates successful touch
+end
+
+-----------------------------------------------------------------------------------------
+-- BEGINNING OF YOUR IMPLEMENTATION
+-- 
+-- NOTE: Code outside of listener functions (below) will only be executed once,
+--		 unless storyboard.removeScene() is called.
+-- 
+-----------------------------------------------------------------------------------------
+
+-- Called when the scene's view does not exist:
+function scene:createScene( event )
+	local group = self.view
+
+	-- display a background image
+	local background = display.newImage( "bg.jpg", true )
+	background.x = display.contentWidth / 2
+	background.y = display.contentHeight / 2
+	
+	-- create/position logo/title image on upper-half of the screen
+	local titleLogo = display.newText("", 0, 0, native.systemFontBold, 36)
+	titleLogo:setReferencePoint( display.CenterReferencePoint )
+	titleLogo.x = display.contentWidth * 0.5
+	titleLogo.y = 100
+	titleLogo.text = "Instablix"
+	
+	-- create a widget button (which will loads level1.lua on release)
+	playBtn = widget.newButton{
+		label="Play Now",
+		labelColor = { default={255}, over={128} },
+		--defaultFile="button.png",
+		--overFile="button-over.png",
+		width=154, height=40,
+		onRelease = onPlayBtnRelease	-- event listener function
+	}
+	playBtn:setReferencePoint( display.CenterReferencePoint )
+	playBtn.x = display.contentWidth*0.5
+	playBtn.y = display.contentHeight - 125
+	
+	-- all display objects must be inserted into group
+	group:insert( background )
+	group:insert( titleLogo )
+	group:insert( playBtn )
+end
+
+-- Called immediately after scene has moved onscreen:
+function scene:enterScene( event )
+	if storyboard.getPrevious() ~= nil then
+		--print("previous screen in mainmenu " ..  storyboard.getPrevious());
+		storyboard.purgeScene(storyboard.getPrevious())
+		storyboard.removeScene(storyboard.getPrevious())			
+	end
+	local group = self.view
+	
+	-- INSERT code here (e.g. start timers, load audio, start listeners, etc.)
+	
+end
+
+-- Called when scene is about to move offscreen:
+function scene:exitScene( event )
+	local group = self.view
+	
+	-- INSERT code here (e.g. stop timers, remove listenets, unload sounds, etc.)
+	
+end
+
+-- If scene's view is removed, scene:destroyScene() will be called just prior to:
+function scene:destroyScene( event )
+	local group = self.view
+	
+	if playBtn then
+		playBtn:removeSelf()	-- widgets must be manually removed
+		playBtn = nil
+	end
+end
+
+-----------------------------------------------------------------------------------------
+-- END OF YOUR IMPLEMENTATION
+-----------------------------------------------------------------------------------------
+
+-- "createScene" event is dispatched if scene's view does not exist
+scene:addEventListener( "createScene", scene )
+
+-- "enterScene" event is dispatched whenever scene transition has finished
+scene:addEventListener( "enterScene", scene )
+
+-- "exitScene" event is dispatched whenever before next scene's transition begins
+scene:addEventListener( "exitScene", scene )
+
+-- "destroyScene" event is dispatched before view is unloaded, which can be
+-- automatically unloaded in low memory situations, or explicitly via a call to
+-- storyboard.purgeScene() or storyboard.removeScene().
+scene:addEventListener( "destroyScene", scene )
+
+-----------------------------------------------------------------------------------------
+
+return scene
